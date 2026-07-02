8dc24f3c03b364f38bd25fca1fe524f26f63f2b0
Cointrader <cointrader1985@gmail.com>
2026-07-02 08:51:22 +0100

    Create Planet Habitat Runner.cpp Slow travel through
    Slow travel through

---
+++ Planet Habitat Runner.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int eventRoll() {
+    return std::rand() % 3;
+}
+
+void showState(int energy, int distance) {
+    std::cout << "\n--- Travel Status ---\n";
+    std::cout << "Energy: " << energy << "\n";
+    std::cout << "Distance: " << distance << "\n";
+}
+
+int main() {
+    std::srand(static_cast<unsigned>(std::time(nullptr)));
+
+    int energy = 6;
+    int distance = 0;
+
+    std::cout << "=== Planet Habitat Runner ===\n";
+
+    while (energy > 0 && distance < 60) {
+        int move;
+
+        std::cout << "\nChoose movement (1-3): ";
+        std::cin >> move;
+
+        int event = eventRoll();
+
+        if (move == 1) {
+            std::cout << "Slow travel through home base.\n";
+            distance += 10;
+        } else if (move == 2) {
+            std::cout << "Urban apartment route selected.\n";
+            distance += 15;
+            energy -= 1;
+        } else {
+            std::cout << "Expanding toward earth exploration path.\n";
+            distance += 20;
+            energy -= 2;
+        }
+
+        if (event == 0)
+            std::cout << "S
